50f3399a6467269b6a59632aea127a7ec282a616
Yury Struchkou <yury.struchkou@gmail.com>
2018-08-06 11:16:50 +0300

    Created basic console app.

---
+++ ConsoleApp/ConsoleProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class ConsoleProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
